c84a86d0d38df8dc416bd0cd037da0f710ef27f7
Bernardo <bernard0.dev@proton.me>
2022-12-07 16:42:31 +0000

    Day 2

---
+++ 2022/2/part-1/point-counting.js
@@ -0,0 +1,53 @@
+const fs = require('node:fs')
+const readline = require('node:readline')
+
+const DECYPHER = {
+    "A": "X",
+    "B": "Y",
+    "C": "Z",
+}
+
+const SHAPE_POINTS = {
+    "X": 1,
+    "Y": 2,
+    "Z": 3,
+}
+
+const WINS = {
+    "A": "Y",
+    "B": "Z",
+    "C": "X"
+}
+
+const calc_result_points = (elf, player) => {
+    if (DECYPHER[elf] === player) return 3
+    if (WINS[elf] === player) return 6
+    return 0
+}
+
+const pointCounting = async() => {
+    const fileStream = fs.createReadStream('../input.txt')
+
+    const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity
+    })
+
+
+    let points = 0
+
+    for await (const line of rl) {
+        let round_points = 0
+
+        let [elf, player] = line.split(' ')
+
+        round_points += SHAPE_POINTS[player]
+
+        round_points += calc_result_points(elf, player)
+
+        points += round_points
+    }
+    console.log(points)
+}
+
+pointCounting()
---
+++ 2022/2/part-2/point-counting-ends.js
@@ -0,0 +1,66 @@
+
+const fs = require('node:fs')
+const readline = require('node:readline')
+
+const DECYPHER = {
+    "A": "X",
+    "B": "Y",
+    "C": "Z",
+}
+
+const SHAPE_POINTS = {
+    "X": 1,
+    "Y": 2,
+    "Z": 3,
+}
+
+const RESULT_DIRECTIONS = {
+    "X": -1,
+    "Y": 0,
+    "Z": 1,
+}
+
+const RESULT_POINTS = {
+    "X": 0,
+    "Y": 3,
+    "Z": 6,
+}
+
+const shapes = ["X", "Y", "Z"]
+
+const mod = (n, m) => {
+    return ((n % m) + m) % m;
+}
+
+const pointCounting = async() => {
+    const fileStream = fs.createReadStream('../input.txt')
+
+    const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity
+    })
+
+
+    let points = 0
+
+    for await (const line of rl) {
+        let round_points = 0
+
+        let [elf, result] = line.split(' ')
+
+        const idx = shapes.indexOf(DECYPHER[elf])
+
+        const player_idx = mod((idx + RESULT_DIRECTIONS[result]), shapes.length)
+
+        const shape_points = SHAPE_POINTS[shapes[player_idx]]
+
+        round_points += shape_points
+
+        round_points += RESULT_POINTS[result]
+
+        points += round_points
+    }
+    console.log(points)
+}
+
+pointCounting()
